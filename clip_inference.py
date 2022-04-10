5a613268d9adcfdaaafa8ec4882ab75431996230
ReneFabricius <fabricius@stud.uniza.sk>
2022-04-10 18:28:42 +0200

    Feature saving in clip inference.

--- clip_inference.py
+++ clip_inference.py
@@ -26,8 +26,11 @@ def infer_clip():
     parser.add_argument('-architecture', type=str, default='ViT-B/32', help='Clip architecture')
     parser.add_argument('-verbosity', default=0, type=int, help='Verbosity level')
     parser.add_argument('-multiple_repl', action="store_true", dest="multi_repl", help="Root folder contains multiple replications folders")
+    parser.add_argument('-load_features', action="store_true", dest="load_feat", help="If specified, training and testing features are loaded. Expected location is the architecture subfolder of the root folder.")
     args = parser.parse_args()
     
+    clip_name = "clip_{}".format(args.architecture.replace('/', '-')) + "_LP"
+
     if args.multi_repl:
         fold_ptrn = r'^\d+$'
         folders = [f for f in os.listdir(args.folder) if os.path.isdir(os.path.join(args.folder, f))]
@@ -37,47 +40,59 @@ def infer_clip():
     else:
         repl_folders = [""]
 
-    print("Loading clip model")
-    model, preprocess = clip.load(args.architecture, device=args.device, download_root=args.clip_data)
-    model.eval().float()
-    
-    print("Loading dataset")
-    if args.dataset == "cifar10":
-        dataset_train = torchvision.datasets.CIFAR10(root=args.dataset_data, train=True, download=True, transform=preprocess)
-        dataset_test = torchvision.datasets.CIFAR10(root=args.dataset_data, train=False, download=True, transform=preprocess)
-    elif args.dataset == "cifar100":
-        dataset_train = torchvision.datasets.CIFAR100(root=args.dataset_data, train=True, download=True, transform=preprocess)
-        dataset_test = torchvision.datasets.CIFAR100(root=args.dataset_data, train=False, download=True, transform=preprocess)
-    elif args.dataset == "imagenet":
-        traindir = os.path.join(args.dataset_data, "train")
-        testdir = os.path.join(args.dataset_data, "val")
-        dataset_train = torchvision.datasets.ImageFolder(root=traindir, transform=preprocess)
-        dataset_test = torchvision.datasets.ImageFolder(root=testdir, transform=preprocess)
-    else:
-        print("Error: unsupported dataset type {}".format(args.dataset))
-        return 1
-    
-    train_loader = DataLoader(dataset=dataset_train, batch_size=args.batch_sz, shuffle=False, num_workers=4, pin_memory=True)
-    test_loader = DataLoader(dataset=dataset_test, batch_size=args.batch_sz, shuffle=False, num_workers=4, pin_memory=True)
-    
-    train_features = []
-    test_features = []
-    
-    print("Processing train data")
-    for batch_index, (images, labels) in enumerate(train_loader):
-        print("Progress {}%".format(100 * (batch_index + 1) // len(train_loader)), end="\r")
-        with torch.no_grad():
-            train_features.append(model.encode_image(images.to(args.device)).cpu())
-    print("\n") 
-    print("Processing test data")
-    for batch_index, (images, labels) in enumerate(test_loader):
-        print("Progress {}%".format(100 * (batch_index + 1) // len(test_loader)), end="\r")
-        with torch.no_grad():
-            test_features.append(model.encode_image(images.to(args.device)).cpu())
-    print("\n") 
+    if not args.load_feat:
+        print("Loading clip model")
+        model, preprocess = clip.load(args.architecture, device=args.device, download_root=args.clip_data)
+        model.eval().float()
+        
+        print("Loading dataset")
+        if args.dataset == "cifar10":
+            dataset_train = torchvision.datasets.CIFAR10(root=args.dataset_data, train=True, download=True, transform=preprocess)
+            dataset_test = torchvision.datasets.CIFAR10(root=args.dataset_data, train=False, download=True, transform=preprocess)
+        elif args.dataset == "cifar100":
+            dataset_train = torchvision.datasets.CIFAR100(root=args.dataset_data, train=True, download=True, transform=preprocess)
+            dataset_test = torchvision.datasets.CIFAR100(root=args.dataset_data, train=False, download=True, transform=preprocess)
+        elif args.dataset == "imagenet":
+            traindir = os.path.join(args.dataset_data, "train")
+            testdir = os.path.join(args.dataset_data, "val")
+            dataset_train = torchvision.datasets.ImageFolder(root=traindir, transform=preprocess)
+            dataset_test = torchvision.datasets.ImageFolder(root=testdir, transform=preprocess)
+        else:
+            print("Error: unsupported dataset type {}".format(args.dataset))
+            return 1
+        
+        train_loader = DataLoader(dataset=dataset_train, batch_size=args.batch_sz, shuffle=False, num_workers=4, pin_memory=True)
+        test_loader = DataLoader(dataset=dataset_test, batch_size=args.batch_sz, shuffle=False, num_workers=4, pin_memory=True)
+        
+        train_features = []
+        test_features = []
+        
+        print("Processing train data")
+        for batch_index, (images, labels) in enumerate(train_loader):
+            print("Progress {}%".format(100 * (batch_index + 1) // len(train_loader)), end="\r")
+            with torch.no_grad():
+                train_features.append(model.encode_image(images.to(args.device)).cpu())
+        print("\n") 
+        print("Processing test data")
+        for batch_index, (images, labels) in enumerate(test_loader):
+            print("Progress {}%".format(100 * (batch_index + 1) // len(test_loader)), end="\r")
+            with torch.no_grad():
+                test_features.append(model.encode_image(images.to(args.device)).cpu())
+        print("\n") 
 
-    train_features = torch.cat(train_features, dim=0).to(args.device)
-    test_features = torch.cat(test_features, dim=0).to(args.device)
+        train_features = torch.cat(train_features, dim=0).to(args.device)
+        test_features = torch.cat(test_features, dim=0).to(args.device)
+
+        if not os.path.exists(os.path.join(args.folder, clip_name)):
+            os.mkdir(os.path.join(args.folder, clip_name))
+
+        np.save(os.path.join(args.folder, clip_name, "train_features.npy"), train_features.cpu())
+        np.save(os.path.join(args.folder, clip_name, "test_features.npy"), test_features.cpu())
+    
+    else:
+        train_features = torch.from_numpy(np.load(os.path.join(args.folder, clip_name, "train_features.npy"))).to(args.device)
+        test_features = torch.from_numpy(np.load(os.path.join(args.folder, clip_name, "test_features.npy"))).to(args.device)
+        
     train_features /= torch.linalg.vector_norm(train_features, dim=-1, keepdim=True)
     test_features /= torch.linalg.vector_norm(test_features, dim=-1, keepdim=True)
         
@@ -118,7 +133,7 @@ def infer_clip():
             if args.verbosity > 0:
                 print("Testing C value {}".format(C_val))
                 
-            log_reg = LogisticRegression(solver="sag", penalty='l2', max_iter=1000, C=C_val, verbose=args.verbosity - 1, multi_class="multinomial")
+            log_reg = LogisticRegression(solver="sag", penalty='l2', max_iter=1000, C=C_val, verbose=args.verbosity, multi_class="multinomial")
             log_reg.fit(X=lin_train_features.cpu(), y=lin_train_tar.cpu())            
             val_pred = torch.tensor(log_reg.decision_function(lin_val_features.cpu()), device=args.device)
             cur_acc = torch.sum(val_pred.topk(k=1, dim=-1).indices.squeeze() == lin_val_tar).item() / len(lin_val_tar)
@@ -134,7 +149,6 @@ def infer_clip():
             test_logits = torch.tensor(best_model.decision_function(test_features.cpu()), device=args.device, dtype=torch.float)
             print("Linear probe inference finished in {}s".format(timer() - start))
 
-        clip_name = "clip_{}".format(args.architecture.replace('/', '-')) + "_LP"
         net_folder = os.path.join(args.folder, repl_f, "outputs", clip_name) 
         
         if not os.path.exists(net_folder): 
